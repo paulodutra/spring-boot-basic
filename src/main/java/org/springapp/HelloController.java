29761e4d58dab1ed91854b98ef5703217ac56078
paulodutra <paulodutra.info@gmail.com>
2022-11-25 12:06:18 -0300

    installing jpa dependencies, configuring application.properties file and create entity using jpa annotations

--- src/main/java/org/springapp/HelloController.java
+++ src/main/java/org/springapp/HelloController.java
@@ -1,15 +1,28 @@
 package org.springapp;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloController {
 
-    @RequestMapping
+    @RequestMapping(path = "/")
     @ResponseBody
     public String hello() {
         return "Hello World by controller";
     }
+
+    @GetMapping(path = "/say/{name}")
+    @ResponseBody
+    public String say(@PathVariable String name) {
+        return "Hello " + name + " this is my new method";
+    }
+
+    @PostMapping(path = "/say")
+    @ResponseBody
+    public String sayPost(@RequestParam String name) {
+        return "This is my result: " + name;
+    }
+
 }
